13ed9f9f51b70108cc1a32d496b1303857f9b4c8
jose145 <56898340+jose145@users.noreply.github.com>
2019-11-23 19:47:36 -0500

    se creo el archivo decodificador9.py

---
+++ Castillo/decodificadores/decodificador9.py
@@ -0,0 +1,34 @@
+import os
+
+# Decodificar mensaje encriptado
+
+msg="ESTERNOCLEIDOMASTOIDEO"
+
+for letra in msg:
+    if letra == "E":
+        print(os.sys.argv[1])
+    if letra == "S":
+        print(os.sys.argv[2])
+    if letra == "T":
+        print(os.sys.argv[3])
+    if letra == "R":
+        print(os.sys.argv[4])
+    if letra == "I":
+        print(os.sys.argv[5])
+    if letra == "N":
+        print(os.sys.argv[6])
+    if letra == "O":
+        print(os.sys.argv[7])
+    if letra == "C":
+        print(os.sys.argv[8])
+    if letra == "D":
+        print(os.sys.argv[9])
+    if letra == "L":
+        print(os.sys.argv[10])
+    if letra == "M":
+        print(os.sys.argv[11])
+    if letra == "A":
+        print(os.sys.argv[12])
+#fin_iterador
+
+print("Fin del bucle")
