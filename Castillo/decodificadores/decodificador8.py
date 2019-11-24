2416409d265afdcf01cd07bbf3251b3c46dad906
jose145 <56898340+jose145@users.noreply.github.com>
2019-11-23 19:46:42 -0500

    se creo el archivo decodificador8.py

---
+++ Castillo/decodificadores/decodificador8.py
@@ -0,0 +1,22 @@
+import os
+
+# Decodificar mensaje encriptado
+
+msg="UEILMEG"
+
+for letra in msg:
+    if letra == "M":
+        print(os.sys.argv[1])
+    if letra == "I":
+        print(os.sys.argv[2])
+    if letra == "G":
+        print(os.sys.argv[3])
+    if letra == "U":
+        print(os.sys.argv[4])
+    if letra == "E":
+        print(os.sys.argv[5])
+    if letra == "L":
+        print(os.sys.argv[6])
+#fin_iterador
+
+print("Fin del bucle")
