654b0871fc312b7a92d46e802018c59fc68954e5
jose145 <56898340+jose145@users.noreply.github.com>
2019-11-23 19:42:43 -0500

    se creo el archivo recorrido10.py

---
+++ Castillo/repetitivas para/recorrido10.py
@@ -0,0 +1,10 @@
+import os
+
+# Imprimir los primeros numeros multiplos de 5
+
+rango= os.sys.argv[1]
+for i in range(int(rango)):
+    print((i+ 0)*5)
+#fin_iterador_en_rango
+
+print("Fin del bucle")
