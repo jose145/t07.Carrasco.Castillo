0d1aff00210da0a7371805bce568f988c2acd4f4
jose145 <56898340+jose145@users.noreply.github.com>
2019-11-23 19:48:16 -0500

    se creo el archivo decodificador10.py

---
+++ Castillo/decodificadores/decodificador10.py
@@ -0,0 +1,20 @@
+import os
+
+# Decodificar mensaje encriptado
+msg="YSDLP"
+
+for letra in msg:
+    if letra == "Y":
+        print(os.sys.argv[1])
+    if letra == "S":
+        print(os.sys.argv[2])
+    if letra == "D":
+        print(os.sys.argv[3])
+    if letra == "L":
+        print(os.sys.argv[4])
+    if letra == "P":
+        print(os.sys.argv[5])
+#fin_iterador
+
+print("Fin del bucle")
+
