d842bcf5ad0ff4fdbacca2c641968ea51dbfcb0e
jose145 <56898340+jose145@users.noreply.github.com>
2019-11-23 19:36:22 -0500

    se creo el archivo lote.bat

---
+++ Castillo/lote.bat
@@ -0,0 +1,24 @@
+python Validadores/Validador6.py 15
+python Validadores/Validador7.py 20 35
+python Validadores/Validador8.py 0 100
+python Validadores/Validador9.py agua fuego tierra aire
+python Validadores/Validador10.py lunes martes miercoles jueves viernes
+
+python repetitivas para/recorrido6.py 300
+python repetitivas para/recorrido7.py 600
+python repetitivas para/recorrido8.py 10
+python repetitivas para/recorrido9.py 150
+python repetitivas para/recorrido10.py 30
+
+python iteracion/iteracion6.py juan
+python iteracion/iteracion7.py 145987
+python iteracion/iteracion8.py 154
+python iteracion/iteracion9.py esternocleidomastoideo
+python iteracion/iteracion10.py 154998515674
+
+python decodificadores/decodificador6.py hola como_estas  bien  mal
+python decodificadores/decodificador7.py Hay_clases Estoy_en_aula Ven_al_laboratorio Vamos_a_comer Adios
+python decodificadores/decodificador8.py Te_veo_luego Tengo_sed Tengo_hambre Vamos_al_cine Tengo_tarea Salida
+python decodificadores/decodificador9.py Hola Estoy_bien Estoy_mal Me_voy Voy_a_dormir Tengo_examen Voy_a_banarme Voy_a_correr Tengo_Fiesta Vamos_a_nadar Te_odio Te_amo
+python decodificadores/decodificador10.py Yo Soy De La Pedro
+
