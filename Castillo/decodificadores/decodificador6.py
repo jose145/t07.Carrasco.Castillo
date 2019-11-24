acc696db3f919a2b3f76ee701b8d45728822816b
jose145 <56898340+jose145@users.noreply.github.com>
2019-11-23 19:45:52 -0500

    se creo el archivo decodificador6.py

---
+++ Castillo/decodificadores/decodificador6.py
@@ -0,0 +1,18 @@
+import os
+
+# Decodificar mensaje encriptado
+msg="JSEOESJ"
+
+for letra in msg:
+    if letra == "J":
+        print(os.sys.argv[1])
+    if letra == "O":
+        print(os.sys.argv[2])
+    if letra == "S":
+        print(os.sys.argv[3])
+    if letra == "E":
+        print(os.sys.argv[4])
+#fin_iterador
+
+print("Fin del bucle")
+
