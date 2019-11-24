a35f5eb018e64b845f6ac5d5851b57cdd17507d5
jose145 <56898340+jose145@users.noreply.github.com>
2019-11-23 19:46:19 -0500

    se creo el archivo decodificador7.py

---
+++ Castillo/decodificadores/decodificador7.py
@@ -0,0 +1,20 @@
+import os
+
+# Decodificar mensaje encriptado
+
+msg="NURPGN"
+
+for letra in msg:
+    if letra == "U":
+        print("Hay clases")
+    if letra == "N":
+        print("Estoy en aula")
+    if letra == "P":
+        print("Ven al laboratorio")
+    if letra == "R":
+        print("Vamos a comer")
+    if letra == "G":
+        print ("Adios")
+#fin_iterador
+
+print("Fin del bucle")
