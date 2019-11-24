e3f98cd0aa1d05f769a1837e5f2f30246596bc1f
jose145 <56898340+jose145@users.noreply.github.com>
2019-11-23 19:36:48 -0500

    se creo el archivo lote.bat

---
+++ Castillo/lote.err.bat
@@ -0,0 +1,24 @@
+python Validadores/Validador6.py 18
+python Validadores/Validador7.py 15 10
+python Validadores/Validador8.py 10 154
+python Validadores/Validador9.py azul verde amarillo
+python Validadores/Validador10.py 1 2 3 4 5
+
+python repetitivas para/recorrido6.py 250
+python repetitivas para/recorrido7.py 800
+python repetitivas para/recorrido8.py 28
+python repetitivas para/recorrido9.py 1400
+python repetitivas para/recorrido10.py 78
+
+python iteracion/iteracion6.py lui
+python iteracion/iteracion7.py 45795
+python iteracion/iteracion8.py 254
+python iteracion/iteracion9.py trapezoide
+python iteracion/iteracion10.py 64df787
+
+python decodificadores/decodificador6.py hola como_estas  bien  mal
+python decodificadores/decodificador7.py Hay_clases Estoy_en_aula Ven_al_laboratorio Vamos_a_comer Adios
+python decodificadores/decodificador8.py Te_veo_luego Tengo_sed Tengo_hambre Vamos_al_cine Tengo_tarea Salida
+python decodificadores/decodificador9.py Hola Estoy_bien Estoy_mal Me_voy Voy_a_dormir Tengo_examen Voy_a_banarme Voy_a_correr Tengo_Fiesta Vamos_a_nadar Te_odio Te_amo
+python decodificadores/decodificador10.py Yo Soy De La Pedro
+
